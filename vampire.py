fe7aa7d9eca74741bbe42a57daf2daa0cb40b7b8
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-09 05:21:50 +0900

    Add files via upload

---
+++ vampire.py
@@ -0,0 +1,11 @@
+name = input("name: ")
+age = int(input("age: "))
+
+if name == "Alice":
+    print("やぁ、Alice。")
+elif age < 12:
+    print("Aliceじゃないね、お嬢ちゃん。")
+elif age > 2000:
+    print("Aliceはお前のような不死身ではない、吸血鬼め。")
+elif age > 100:
+    print("Aliceじゃないね。お婆ちゃん。")
