fd92da3a77355a60e66b58f3e11a489ad9c5c8b2
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-09 05:22:57 +0900

    Add files via upload

---
+++ 2章/littleKid.py
@@ -0,0 +1,9 @@
+name = input("name: ")
+age = int(input("age: "))
+
+if name == "Alice":
+    print("やぁ、Alice。")
+elif age < 12:
+    print("Aliceじゃないね、お嬢ちゃん。")
+else:
+    print("君はAliceでも子供でもないね。")
