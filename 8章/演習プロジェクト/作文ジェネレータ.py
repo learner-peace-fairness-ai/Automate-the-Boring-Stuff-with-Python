37d21554b115ca919e35f4f52d7f5e58d727c02c
wh <99289136+wh-Pythonista@users.noreply.github.com>
2025-01-18 22:31:48 +0900

    Update 作文ジェネレータ.py

--- 8章/演習プロジェクト/作文ジェネレータ.py
+++ 8章/演習プロジェクト/作文ジェネレータ.py
@@ -2,15 +2,6 @@
 
 import re
 
-
-def is_vowel(word):
-    regex = re.compile(r'[AIUEO]+')
-    if regex.match(word):
-        return True
-    else:
-        return False
-
-
 text = 'The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was unaffected by these events.'
 
 part_of_speech_regex = re.compile(r'ADJECTIVE|ADVERB|NOUN|VERB')
@@ -20,7 +11,7 @@ while part_of_speech_regex.search(s):
     mo = part_of_speech_regex.search(s)
     part_of_speech = mo.group()
     
-    if is_vowel(part_of_speech):
+    if part_of_speech.startswith(('A', 'I', 'U', 'E', 'O')):
         replacement = input(f'Enter an {part_of_speech.lower()}: \n')
     else:
         replacement = input(f'Enter a {part_of_speech.lower()}: \n')
